ae39c30630692edc6765294fea1393994d3c52ce
Eugenio <eugenio.sanchez@gmail.com>
2021-02-22 18:05:45 +0100

    1.4: course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -17,10 +17,10 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 const Part = (props) => {
-  console.log(props.part);
+  console.log(props.parts);
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {props.parts.name} {props.parts.exercises}
     </p>
   );
 };
@@ -28,37 +28,46 @@ const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part parts={props.parts[0]} />
+      <Part parts={props.parts[1]} />
+      <Part parts={props.parts[2]} />
     </div>
   );
 };
-const Total = ({ amount }) => {
-  console.log({ amount });
-  return <p> Number of exercises {amount} </p>;
+const Total = (props) => {
+  console.log(props);
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
 };
 const App = () => {
   //Const
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total amount={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
