f898bb502cca812d20df02da43f238229b4efd1b
Eugenio <eugenio.sanchez@gmail.com>
2021-02-25 10:37:57 +0100

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,16 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const Counter = (props) => (
-    <button onClick={props.handleClick}>{props.title}</button>
-  );
-
+const Statistics = ({ good, bad, neutral }) => {
   const total = ({ good, bad, neutral }) => good + bad + neutral;
 
   const average = ({ good, bad, neutral }) => {
@@ -25,11 +16,6 @@ const App = () => {
 
   return (
     <div>
-      <h2>give feedback</h2>
-      <br />
-      <Counter handleClick={() => setGood(good + 1)} title="good" />
-      <Counter handleClick={() => setNeutral(neutral + 1)} title="neutral" />
-      <Counter handleClick={() => setBad(bad + 1)} title="bad" />
       <h2>statistics</h2>
       <br />
       good {good}
@@ -43,6 +29,28 @@ const App = () => {
       average {average({ good, bad, neutral })}
       <br />
       positive {positive({ good, bad, neutral })}
+    </div>
+  );
+};
+
+const Counter = (props) => (
+  <button onClick={props.handleClick}>{props.title}</button>
+);
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <br />
+      <Counter handleClick={() => setGood(good + 1)} title="good" />
+      <Counter handleClick={() => setNeutral(neutral + 1)} title="neutral" />
+      <Counter handleClick={() => setBad(bad + 1)} title="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
