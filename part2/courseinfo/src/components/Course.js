c34ad4edfaa53e25e417ca93a269f6bb7421727e
Eugenio <eugenio.sanchez@gmail.com>
2021-02-27 18:01:46 +0100

    2.2: Course information step7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -6,30 +6,13 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-/*
-Wrong Solution
-array.map ((item,i)=> <li key={i}> {item.name}</li>)
-
-Better Solution
-let counter = 1
-array.map ((item)=> <li key={counter++}> {item.name}</li>)
-
-Much Better Production solution
-import { nanoid } from 'nanoid';
-const createNewTodo = (text) => ({
-  completed: false,
-  id: nanoid(),
-  text
-}
-*/
-
 const Content = ({ parts }) => {
   console.log(parts);
-  let todoCounter = 0;
+
   return (
     <div>
       {parts.map((part) => (
-        <p key={todoCounter++}>
+        <p key={part.id}>
           {part.name} {part.exercises}
         </p>
       ))}
@@ -37,24 +20,20 @@ const Content = ({ parts }) => {
   );
 };
 
-/* const Total = (props) => {
-  console.log(props);
-  return (
-    <p>
-      Number of exercises{' '}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-}; */
+const Total = ({ parts }) => {
+  console.log(parts);
+  let total = 0;
+  parts.map((item) => (total += item.exercises));
+
+  return <b>Total of {total} exercises</b>;
+};
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      {/* <Total parts={course.parts} /> */}
+      <Total parts={course.parts} />
     </div>
   );
 };
