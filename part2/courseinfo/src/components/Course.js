5339f8572d6c3323fedb9b63b648538e7b4545e9
Eugenio <eugenio.sanchez@gmail.com>
2021-02-27 21:45:47 +0100

    2.4: Course information step9 & 2.5: separate module

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -21,17 +21,22 @@ const Content = ({ parts }) => {
 };
 
 const Total = ({ parts }) => {
-  console.log(parts);
-  const exercises = parts.map((item) => item.exercises);
+  console.log('parts', parts);
+
+  /*const exercises = parts.map((item) => item.exercises);
   const total = exercises.reduce((s, p) => {
     console.log('what is happening', s, p);
     return s + p;
   });
+ */
 
+  //Const total = reduce (previous, current) => sum,0 initial value
+  const total = parts.reduce((pre, current) => pre + current.exercises, 0);
   return <b>Total of {total} exercises</b>;
 };
 
 const Course = ({ course }) => {
+  console.log(course);
   return (
     <div>
       <Header course={course.name} />
