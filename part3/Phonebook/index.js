cf92408126d1f6f604d237c5181df09eafa824b3
Eugenio <eugenio.sanchez@gmail.com>
2021-03-12 17:27:51 +0100

    3.3: Phonebook backend step3

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -22,6 +22,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
 app.get('/info', (request, response) => {
   const entries = persons.length;
   const dateinfo = new Date();
